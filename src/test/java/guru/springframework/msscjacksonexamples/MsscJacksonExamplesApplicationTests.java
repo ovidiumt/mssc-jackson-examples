73496d051c7f220f7dafc414f14d6b373002886c
Tudorache Mihai-Ovidiu <ovi99or@gmail.com>
2023-06-12 16:21:37 +0300

    initial commit

---
+++ src/test/java/guru/springframework/msscjacksonexamples/MsscJacksonExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.msscjacksonexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscJacksonExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
