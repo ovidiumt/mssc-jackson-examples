51d7f2b646776c6006641619ddb597770d180899
Tudorache Mihai-Ovidiu <ovi99or@gmail.com>
2023-06-12 17:49:29 +0300

    added json serializet

--- src/main/java/guru/springframework/msscjacksonexamples/model/BeerDto.java
+++ src/main/java/guru/springframework/msscjacksonexamples/model/BeerDto.java
@@ -2,6 +2,7 @@ package guru.springframework.msscjacksonexamples.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Null;
 import jakarta.validation.constraints.Positive;
@@ -11,6 +12,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 @Data
@@ -33,4 +35,6 @@ public class BeerDto {
     @JsonFormat(pattern = "yyyy-MM-dd", shape = JsonFormat.Shape.STRING)
     private OffsetDateTime createdDate;
     private OffsetDateTime lastUpdatedDate;
+    @JsonSerialize(using = LocalDateSerializer.class)
+    private LocalDate myLocalDate;
 }
